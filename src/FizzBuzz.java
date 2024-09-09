41d4fd57569d24d2256eede5d0d9cc00f374e039
skapc146 <skapcan52@gmail.com>
2024-09-09 12:06:03 -0400

    'fizz buzz algorithm in java'

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,34 +1,32 @@
 /**
  * Solve the FizzBuzz challenge.
  */
-class FizzBuzz {
+class FizzBuzz
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int i = 0;
 
-        for (int i = 1; i < 100; i++) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
+        while (i < 100)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
                 System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
             }
+            else if (i % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+            i++;
         }
     }
 }
